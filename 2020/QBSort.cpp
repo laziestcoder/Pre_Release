964f08be19ebd30963f6bfe0869d56da143497f9
Towfiq <towfiq.106@gmail.com>
2020-03-05 01:21:37 +0600

    QBSort tested and complexity calculated

--- 2020/QBSort.cpp
+++ 2020/QBSort.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void QBSort(int A[1000], int n){
-	bool k = false, m = 0, i = 0;
-	for (i = 0, j = n-1; i <= j; i++, j--){
+int*  QBSort(int A[1000], int n){
+	bool k = false;
+	int m = 0, i = 0, j = 0;
+	for (i = 0, j = n-1; i <= j; i++, j--){ // This loop runs 'n-1' time . So we can consider it as 'n' times
 		if( A[i] > A[j] ){
 			int t;
 			t = A[i];
@@ -12,7 +13,7 @@ void QBSort(int A[1000], int n){
 		}
 	}
 
-	for(i = 0, j = n-1; i <= j; i++, j-- ){
+	for(i = 0, j = n-1; i <= j; i++, j-- ){   // This loop also runs 'n' time
 		if( A[i] <= A[i+1] && A[j] >= A[j-1] ){
 			continue;
 		}else{
@@ -20,9 +21,9 @@ void QBSort(int A[1000], int n){
 		}
 	}
 
-	while( k ){
+	while( k ){ // This can be iterate at max 'm' times
 		k = false;
-		for( i = 0; i < n-1; i++ ){
+		for( i = 0; i < n-1; i++ ){ // This loop also runs 'n' time
 			if( A[i] > A[i+1] ){
 				int t;
 				t = A[i];
@@ -31,11 +32,20 @@ void QBSort(int A[1000], int n){
 				k = true;
 			}
 		}
+		m++;
+
+		//So this while loop can iterate 'm' times and for loop 'n' times
+		//the relation between while and for loop is => while * for
+		//So the Complexity is m*n, and we can consider m=n, so it will become n*n=> n^2 [n square] 
 	}
 
-	m++;
+	return A;
 }
 
+/*	We will cosider the highest value. So the highest value is 'n^2' and the complexity is 
+*	O(n*n)=O(n^2) , it is called 'Big-O-n-square'. It means the time complexity is 'n^2' and depends on value of 'n'.
+*
+*/
 int main (){
 	int n = 0, i, A[1000];
 	cin >> n;
@@ -43,10 +53,10 @@ int main (){
 		cin >> A[i];
 	}
 
-	QBSort(A, n);
+	int *B = QBSort(A, n);
 
 	for (i = 0; i < n; i++ ){
-		cout << A[i] << " " ;
+		cout << B[i] << " " ;
 	}
 
 
